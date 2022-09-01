ce0e11187e99acd2c80630c1dabc9b29e2d6964e
jesse <jesse@jessegarcia.me>
2022-09-01 11:58:20 -0500

    new project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script>
+    export default {
+      methods: {
+        sup(){
+          alert('click')
+        }
+      }
+    };
+</script>
+
+<template>
+  <slot></slot>
+  <button @click="sup">watched</button>
+  sup
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+const app = createApp(App)
+
+app.component('hello-world', HelloWorld);
+
+
+app.mount('#app')
